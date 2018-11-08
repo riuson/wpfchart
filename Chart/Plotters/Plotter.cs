464ddacc1951316c80d9a79553288365b94082f3
Vladimir <riuson@gmail.com>
2018-11-08 22:10:15 +0500

    Base plotter

---
+++ Chart/Plotters/Plotter.cs
@@ -0,0 +1,13 @@
+﻿using Chart.Series;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace Chart.Plotters {
+    public class Plotter : Panel {
+        public Plotter() {
+            this.Series = null;
+        }
+
+        public IEnumerable<ISerie> Series { get; set; }
+    }
+}
