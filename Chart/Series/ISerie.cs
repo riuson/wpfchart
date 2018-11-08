90da3db4e7decdf63e7b55bc0e96fc8fb9229144
Vladimir <riuson@gmail.com>
2018-11-08 20:56:07 +0500

    Added serie's base class

---
+++ Chart/Series/ISerie.cs
@@ -0,0 +1,11 @@
+﻿using Chart.Points;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Chart.Series {
+    public interface ISerie : IEnumerable<IPoint> {
+        string Title { get; }
+        Brush LineBrush { get; }
+        double LineWidth { get; }
+    }
+}
---
+++ Chart/Series/Serie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Chart.Points;
+using System.Collections.ObjectModel;
+using System.Drawing;
+
+namespace Chart.Series {
+    public class Serie : ObservableCollection<IPoint>, ISerie {
+        public string Title { get; set; }
+        public Brush LineBrush { get; set; }
+        public double LineWidth { get; set; }
+
+        public Serie() {
+            this.Title = String.Empty;
+            this.LineBrush = Brushes.Black;
+            this.LineWidth = 1;
+        }
+    }
+}
