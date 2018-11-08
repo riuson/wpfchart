f38645b6d49f9dabff7e3ebb7e943df8a0836581
Vladimir <riuson@gmail.com>
2018-11-08 23:31:27 +0500

    Add geometries to path

--- Chart/Plotters/Plotter.cs
+++ Chart/Plotters/Plotter.cs
@@ -4,20 +4,33 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace Chart.Plotters {
     public class Plotter : Panel {
-        private IEnumerable<GeometryGroup> _geometryGroups;
+        private Path _visualizedSeries;
 
         public Plotter() {
             this.Series = null;
-            this._geometryGroups = null;
+            this._visualizedSeries = new Path() {
+                Stroke = Brushes.Blue,
+                StrokeThickness = 3
+            };
+            this.Children.Add(this._visualizedSeries);
         }
 
         public IEnumerable<ISerie> Series { get; set; }
 
         protected override Size MeasureOverride(Size availableSize) {
             var geometryGroups = this.Series?.Select(serie => serie.Visualizer.GetGeometryGroup(serie, availableSize)).ToArray();
+
+            var combinedGroup = new GeometryGroup();
+
+            foreach (var group in geometryGroups) {
+                combinedGroup.Children.Add(group);
+            }
+
+            this._visualizedSeries.Data = combinedGroup;
 
             return availableSize;
         }
