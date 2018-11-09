5907bce226644ca18edf9ab5def5325d1d17a80a
Vladimir <riuson@gmail.com>
2018-11-09 19:23:33 +0500

    Added method with test data

--- WpfAppTest/MainWindow.xaml.cs
+++ WpfAppTest/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Chart.Points;
 using Chart.Series;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 
@@ -9,6 +12,9 @@ namespace WpfAppTest {
     /// </summary>
     public partial class MainWindow : Window {
         private Serie _serie1;
+        private Task _task;
+        private CancellationTokenSource _token;
+        private IProgress<Tuple<double>> _progress;
 
         public MainWindow() {
             this.InitializeComponent();
@@ -22,6 +28,27 @@ namespace WpfAppTest {
             this._serie1.Add(new PointDouble(1, 1));
 
             this.plotter1.Series = new ISerie[] { this._serie1 };
+
+            this._token = new CancellationTokenSource();
+            this._progress = new Progress<Tuple<double>>(item => this._serie1.Add(new PointDateTime(DateTime.Now, item.Item1)));
+            this._task = Task.Factory.StartNew(o => this.Method((CancellationToken)o, this._progress), this._token.Token, this._token.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+
+            this.Closing += this.MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            this._token.Cancel();
+            this._task.Wait();
+        }
+
+        private void Method(CancellationToken cancellationToken, IProgress<Tuple<double>> progress) {
+            var rnd = new Random();
+            this._serie1.Clear();
+
+            while (!cancellationToken.IsCancellationRequested) {
+                progress.Report(new Tuple<double>(0x800000 + rnd.NextDouble() * 0x100000));
+                cancellationToken.WaitHandle.WaitOne(500);
+            }
         }
     }
 }
