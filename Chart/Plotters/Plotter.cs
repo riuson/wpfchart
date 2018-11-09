7c5b8872782e9006983019213a6e9667954506b9
Vladimir <riuson@gmail.com>
2018-11-09 19:43:42 +0500

    Redraw line when point added

--- Chart/Plotters/Plotter.cs
+++ Chart/Plotters/Plotter.cs
@@ -1,5 +1,6 @@
 ﻿using Chart.Series;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,18 +9,32 @@ using System.Windows.Shapes;
 
 namespace Chart.Plotters {
     public class Plotter : Panel {
-        private Path _visualizedSeries;
+        private Path mVisualizedSeries;
+        private IEnumerable<ISerie> mSeries;
 
         public Plotter() {
-            this.Series = null;
-            this._visualizedSeries = new Path() {
+            this.mSeries = new ISerie[] { };
+            this.mVisualizedSeries = new Path() {
                 Stroke = Brushes.Blue,
                 StrokeThickness = 3
             };
-            this.Children.Add(this._visualizedSeries);
+            this.Children.Add(this.mVisualizedSeries);
         }
 
-        public IEnumerable<ISerie> Series { get; set; }
+        public IEnumerable<ISerie> Series {
+            get => this.mSeries;
+            set {
+                foreach (var serie in this.mSeries.OfType<INotifyCollectionChanged>()) {
+                    serie.CollectionChanged -= this.OnSerieCollectionChanged;
+                }
+
+                this.mSeries = value;
+
+                foreach (var serie in this.mSeries.OfType<INotifyCollectionChanged>()) {
+                    serie.CollectionChanged += this.OnSerieCollectionChanged;
+                }
+            }
+        }
 
         protected override Size MeasureOverride(Size availableSize) {
             var geometryGroups = this.Series?.Select(serie => serie.Visualizer.GetGeometryGroup(serie, availableSize)).ToArray();
@@ -30,7 +45,7 @@ namespace Chart.Plotters {
                 combinedGroup.Children.Add(group);
             }
 
-            this._visualizedSeries.Data = combinedGroup;
+            this.mVisualizedSeries.Data = combinedGroup;
 
             return availableSize;
         }
@@ -41,6 +56,10 @@ namespace Chart.Plotters {
             }
 
             return finalSize;
+        }
+
+        private void OnSerieCollectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
+            this.InvalidateMeasure();
         }
     }
 }
