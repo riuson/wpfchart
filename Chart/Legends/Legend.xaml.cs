51873de19481a6a8eabac4050d94962ca99523bf
Vladimir <riuson@gmail.com>
2018-11-17 19:16:10 +0500

    Added legend

---
+++ Chart/Legends/Legend.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Chart.Series;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Chart.Legends {
+    /// <summary>
+    /// Логика взаимодействия для Legend.xaml
+    /// </summary>
+    public partial class Legend : UserControl {
+        #region Dependency properties
+        public static readonly DependencyProperty SeriesProperty;
+
+        static Legend() {
+
+            SeriesProperty = DependencyProperty.Register("Series",
+                typeof(IEnumerable<ISerie>), typeof(Legend),
+                new FrameworkPropertyMetadata(
+                    null,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure));
+        }
+        #endregion
+
+        public Legend() {
+            this.InitializeComponent();
+        }
+
+        public IEnumerable<ISerie> Series {
+            get => this.GetValue(Legend.SeriesProperty) as IEnumerable<ISerie>;
+            set => this.SetValue(Legend.SeriesProperty, value);
+        }
+    }
+}
